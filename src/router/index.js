0b901a2f3d81728ec60c36cad06f389b128acf06
CruzedV <mr.dobrikv@mail.com>
2023-09-10 18:44:03 +0300

    fix: correct vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
 import CharacterView from "../views/CharactersView.vue"
 import NotFound from "../views/NotFound.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   BASE_URL: "/",
   routes: [
     {
